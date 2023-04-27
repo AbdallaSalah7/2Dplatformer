9c67d5f32202c458d8d01ffffdacde8745f98b25
AbdallaSalah7 <113334983+AbdallaSalah7@users.noreply.github.com>
2023-04-27 23:10:24 +0400

    Yes

---
+++ mainjunior/Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator anim;
+    [SerializeField] private float AttackCoolDown;
+     [SerializeField] private Transform pencilPoint;
+     [SerializeField] private GameObject[] pencils;
+    //private PlayerControl playerControl;
+    private float cooldownTimer = Mathf.Infinity;
+    private void Awake()
+    {
+        //playerControl = GetComponent<PlayerControl>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && cooldownTimer > AttackCoolDown && PlayerControl.instance.canAttack())
+        {
+            Attack();
+        }
+        cooldownTimer += Time.deltaTime;
+
+    }
+    private void Attack()
+    {
+        cooldownTimer = 0;
+
+        pencils[FindPencils()].transform.position = pencilPoint.position;
+        pencils[FindPencils()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+    private int FindPencils()
+    {
+        for (int i = 0; i < pencils.Length; i++)
+        {
+            if (!pencils[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+
+}
