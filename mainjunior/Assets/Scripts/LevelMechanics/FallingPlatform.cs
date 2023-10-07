f84446c192e78e0ac59dc207c27ffe4c19c8667a
AbdallaSalah7 <113334983+AbdallaSalah7@users.noreply.github.com>
2023-10-07 12:09:22 +0400

    ..

--- mainjunior/Assets/Scripts/LevelMechanics/FallingPlatform.cs
+++ mainjunior/Assets/Scripts/LevelMechanics/FallingPlatform.cs
@@ -8,6 +8,7 @@ public class FallingPlatform : MonoBehaviour
     [SerializeField] private float destroyDelay = 2f;
 
     private bool falling = false;
+    //private Vector2 initialPos;
     
     [SerializeField] private Rigidbody2D rb;
 
@@ -27,11 +28,17 @@ public class FallingPlatform : MonoBehaviour
 
         rb.bodyType = RigidbodyType2D.Dynamic;
         Destroy(gameObject, destroyDelay);
+        yield return new WaitForSeconds(5f);
+        /* falling = false;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        yield return new WaitForSeconds(2f);
+        transform.position = initialPos; */
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //initialPos = transform.position;
     }
 
     // Update is called once per frame
