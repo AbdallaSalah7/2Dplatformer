739a863ec921a24c0771e23f0d798bbe62b0c971
SihamAlKousa <113013665+SihamAlKousa@users.noreply.github.com>
2023-10-07 12:53:35 +0400

    ye

--- mainjunior/Assets/Scripts/LevelMechanics/FallingPlatform.cs
+++ mainjunior/Assets/Scripts/LevelMechanics/FallingPlatform.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     [SerializeField] private float fallDelay = 1f;
-    [SerializeField] private float destroyDelay = 2f;
+    //[SerializeField] private float destroyDelay = 2f;
 
     private bool falling = false;
-    //private Vector2 initialPos;
+    private Vector2 initialPos;
     
     [SerializeField] private Rigidbody2D rb;
+
+    private void Start() {
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        initialPos = transform.position;
+    }
+    
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if(falling)
@@ -21,25 +28,24 @@ public class FallingPlatform : MonoBehaviour
         }
     }
 
+
     private IEnumerator StartFall(){
         falling = true;
 
         yield return new WaitForSeconds(fallDelay);
-
-        rb.bodyType = RigidbodyType2D.Dynamic;
-        Destroy(gameObject, destroyDelay);
+        rb.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+        //rb.bodyType = RigidbodyType2D.Dynamic;
+        //Destroy(gameObject, destroyDelay);
         yield return new WaitForSeconds(5f);
+        falling = false;
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        transform.position = initialPos;
         /* falling = false;
         rb.bodyType = RigidbodyType2D.Kinematic;
         yield return new WaitForSeconds(2f);
         transform.position = initialPos; */
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //initialPos = transform.position;
-    }
 
     // Update is called once per frame
     void Update()
