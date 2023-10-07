2a057a8278902f69eb86ca7facdd9141d2bb7bad
AbdallaSalah7 <113334983+AbdallaSalah7@users.noreply.github.com>
2023-10-07 13:44:37 +0400

    chandelier and thwomp

---
+++ mainjunior/Assets/Scripts/Traps/Chandelier.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chandelier : DamagePlayer
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float range;
+    [SerializeField] private float checkDelay;
+    [SerializeField] private LayerMask player;
+    private float checkTimer;
+    private Vector3 destination;
+
+    private bool attacking;
+
+    private Vector3 direction = new Vector3();
+
+    private void Update() {
+        if(attacking)
+            transform.Translate(destination * Time.deltaTime * speed);
+        else{
+            checkTimer += Time.deltaTime;
+            if(checkTimer > checkDelay)
+            CheckForPlayer();
+        }
+    }
+
+    private void CheckForPlayer(){
+        CalculateDirections();
+
+        Debug.DrawRay(transform.position, direction, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, range, PlayerLayer);
+
+        if(hit.collider != null && !attacking){
+            attacking = true;
+            destination = direction;
+            checkTimer = 0;
+        }
+    }
+
+    private void CalculateDirections(){
+        direction = -transform.up * range;
+        
+    }
+}
