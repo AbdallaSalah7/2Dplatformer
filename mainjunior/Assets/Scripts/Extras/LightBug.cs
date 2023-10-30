487a85089f2e1ac5f925ad0675f4a7dcdbfe5b18
AlaaLaghoub <113013668+AlaaLaghoub@users.noreply.github.com>
2023-10-30 17:33:56 +0400

    edits

---
+++ mainjunior/Assets/Scripts/Extras/LightBug.cs
@@ -0,0 +1,88 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class LightBug : MonoBehaviour
+// {
+//     // Start is called before the first frame updatepublic Transform[] points;
+//     public float moveSpeed;
+//     private int currentPoint;
+
+//     public SpriteRenderer BugSr;
+
+//     public float distanceToAttackPlayer, chaseSpeed;
+
+//     private Vector3 attackTarget;
+
+//     public float waitAfterAttack;
+//     private float attackCounter;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         for (int i = 0; i < points.Length; i++)
+//         {
+//             points[i].parent = null;
+//         }
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (attackCounter > 0)
+//         {
+//             attackCounter -= Time.deltaTime;
+//         }
+//         else
+//         {
+
+//             if (Vector3.Distance(transform.position, PlayerControl.instance.transform.position) > distanceToAttackPlayer)
+//             {
+
+//                 attackTarget = Vector3.zero;
+
+//                 transform.position = Vector3.MoveTowards(transform.position, points[currentPoint].position, moveSpeed * Time.deltaTime);
+
+//                 if (Vector3.Distance(transform.position, points[currentPoint].position) < .05f)
+//                 {
+//                     currentPoint++;
+
+//                     if (currentPoint >= points.Length)
+//                     {
+//                         currentPoint = 0;
+//                     }
+//                 }
+
+//                 // if (transform.position.x < points[currentPoint].position.x)
+//                 // {
+//                 //     BatSR.flipX = true;
+//                 // }
+//                 // else if (transform.position.x > points[currentPoint].position.x)
+//                 // {
+//                 //     BatSR.flipX = false;
+//                 // }
+//             }
+//             else
+//             {
+//                 //Attacking the Player
+
+//                 if (attackTarget == Vector3.zero)
+//                 {
+//                     attackTarget = PlayerControl.instance.transform.position;
+//                 }
+
+//                 transform.position = Vector3.MoveTowards(transform.position, attackTarget, chaseSpeed * Time.deltaTime);
+
+//                 if (Vector3.Distance(transform.position, attackTarget) <= .1f)
+//                 {
+//                     attackCounter = waitAfterAttack;
+//                     attackTarget = Vector3.zero;
+//                 }
+//             }
+
+//         }
+
+//     }
+
+
+// }
