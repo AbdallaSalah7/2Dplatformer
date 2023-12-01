58728e7b33486a0a03bb9c7860c5db067312e417
AbdallaSalah7 <113334983+AbdallaSalah7@users.noreply.github.com>
2023-12-01 20:03:38 +0400

    yea

--- mainjunior/Assets/Scripts/Enemy/AI stuff/FollowEnemy.cs
+++ mainjunior/Assets/Scripts/Enemy/AI stuff/FollowEnemy.cs
@@ -10,6 +10,7 @@ public class FollowEnemy : EnemyDamage
     public float minDistance;
 
     public Animator anim;
+    [SerializeField] private BoxCollider2D boxcollider;
 
     private void Awake() {
         anim = GetComponent<Animator>();
@@ -24,7 +25,7 @@ public class FollowEnemy : EnemyDamage
             transform.localScale = new Vector3(-1, 1, 1); // Flip to face left
         else if (direction.x > 0)
             transform.localScale = new Vector3(1, 1, 1); // Face right
-            
+
         if(Vector2.Distance(transform.position, target.position) > minDistance){
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
             anim.SetBool("chase", true);
@@ -32,6 +33,19 @@ public class FollowEnemy : EnemyDamage
         else{
             anim.SetBool("chase", false);
         }
+
+        if (GetComponent<Health>().dead == true)
+            {
+                print("test");
+                boxcollider.enabled = false;
+                speed = 0;
+                StartCoroutine(Des());
+            }
+
+            IEnumerator Des(){
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
         
     }
 }
